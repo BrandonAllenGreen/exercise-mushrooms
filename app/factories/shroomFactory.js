eadeca3adf219c9785bf8725d7a98f389f343eeb
BrandonAllenGreen <green04@gmail.com>
2018-01-29 12:30:46 -0600

    updated folder structure

---
+++ app/factories/shroomFactory.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// angular.module("mushroomStuff").factory("ShroomFactory", function($q, $http) {
+
+//   let getShrooms = () => {
+//     return $q( (resolve, reject) => {
+//       $http
+//       .get("https://mushroom-exercise-33fdd.firebaseio.com.json")
+//       .then( (mushrooms) => {
+//         resolve(mushrooms);
+//         console.log("data", mushrooms);
+        
+//       })
+//       .catch( (err) => {
+//         reject(err);
+//       });
+//     });
+//   };
+
+//   return { getShrooms };
+
+// });
